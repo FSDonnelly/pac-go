bb8a8a84d3e069cb246f447fa9c05eb6de601349
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-17 20:23:37 -0700

    unresolved "Unable to activate cbreak mode terminal: exec:..."

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 )
 
 func loadMaze() error {
@@ -25,15 +26,59 @@ func loadMaze() error {
 
 var maze []string
 
+func clearScreen() {
+	fmt.Printf("\x1b[2J")
+	moveCursor(0, 0)
+}
+
+func moveCursor(row, col int) {
+	fmt.Printf("\x1b[%d;%df", row+1, col+1)
+}
+
 func printScreen() {
 	for _, line := range maze {
 		fmt.Println(line)
 	}
 }
 
+func readInput() (string, error) {
+	buffer := make([]byte, 100)
+
+	cnt, err := os.Stdin.Read(buffer)
+	if err != nil {
+		return "", err
+	}
+
+	if cnt == 1 && buffer[0] == 0x1b {
+		return "ESC", nil
+	}
+
+	return "", nil
+}
+
+func init() {
+	cbTerm := exec.Command("/bin/stty", "cbreak", "-echo")
+	cbTerm.Stdin = os.Stdin
+
+	err := cbTerm.Run()
+	if err != nil {
+		log.Fatalf("Unable to activate cbreak mode terminal: %v\n", err)
+	}
+}
+
+func cleanup() {
+	cookedTerm := exec.Command("/bin/stty", "-cbreak", "echo")
+	cookedTerm.Stdin = os.Stdin
+
+	err := cookedTerm.Run()
+	if err != nil {
+		log.Fatalf("Unable to activate cooked mode terminal: %v\n", err)
+	}
+}
+
 func main() {
 	// initialize game
-
+	defer cleanup()
 	// load resources
 	err := loadMaze()
 	if err != nil {
@@ -45,16 +90,19 @@ func main() {
 		// update screen
 		printScreen()
 		// process input
-
+		input, err := readInput()
+		if err != nil {
+			log.Printf("Error reading input: %v", err)
+			break
+		}
 		// process movement
 
 		// process collisions
 
 		// check game over
-
-		// Temp: break infinite loop
-		break
-
+		if input == "ESC" {
+			break
+		}
 		// repeat
 	}
 }
